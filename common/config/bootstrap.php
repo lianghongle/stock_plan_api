d6b38df82471f13ef3168086f128f14254219d49
神圣小树 <lianghogle@163.com>
2019-04-09 17:41:36 +0800

    init

---
+++ common/config/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+Yii::setAlias('@common', dirname(__DIR__));
+
+Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/app_frontend');
+Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/app_backend');
+Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/app_api');
+
+Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
